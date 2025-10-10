e68744ada7b4b10ee8cf087d5aeb6c6c611d0327
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-09 21:00:16 -0300

    Create EnderecoController.java

---
+++ src/main/java/com/crud/clientes/controller/EnderecoController.java
@@ -0,0 +1,42 @@
+package com.crud.clientes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.crud.clientes.dto.EnderecoDTO;
+import com.crud.clientes.service.EnderecoService;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class EnderecoController {
+	public EnderecoService service;
+	
+	public EnderecoController(EnderecoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/cadastrar/endereco")
+	public String formulario(Model model) {
+		model.addAttribute("enderecoDTO", new EnderecoDTO());
+		return "cadastro_endereco";
+	}
+	
+	@PostMapping("/cadastrar/endereco")
+	public String salvarEndereco(@ModelAttribute("enderecoDTO") @Valid EnderecoDTO enderecoDTO, BindingResult result) {
+		if(result.hasErrors()) {
+			return "cadastro_endereco";
+		}
+		service.salvarEndereco(enderecoDTO);
+		return "redirect:/cadastrar/endereco";
+	}
+	//Salvar cadastro: ("/cadastrar/endereco")
+	//Lista de endereços: ("/enderecos")
+	//Editar cadastro: ("/enderecos/editar/{id}")
+	//Atualizar cadastro: ("/enderecos/atualizar/{id}")
+	//Deletar cadastro: ("enderecos/excluir/{id}")
+}
